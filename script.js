64e444032e1700ee779163073aabf72e3b7121aa
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-13 21:20:26 +0200

    Resetting the Game

--- script.js
+++ script.js
@@ -13,15 +13,31 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-const scores = [0, 0];
+let scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 let playing = true;
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+function init() {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    player0El.classList.remove("player--winner");
+    player1El.classList.remove("player--winner");
+    player0El.classList.add("player--active");
+    player1El.classList.remove("player--active");
+
+    diceEl.classList.remove("hidden");
+}
+
+init();
 
 function switchPlayer() {
     // Switch to next player
@@ -66,7 +82,7 @@ btnHold.addEventListener("click", function () {
             scores[activePlayer];
 
         // 2. Check if player's score is >= 100
-        if (scores[activePlayer] >= 10) {
+        if (scores[activePlayer] >= 50) {
             // Finish the game
             playing = false;
             diceEl.classList.add("hidden");
@@ -83,3 +99,5 @@ btnHold.addEventListener("click", function () {
         }
     }
 });
+
+btnNew.addEventListener("click", init);
